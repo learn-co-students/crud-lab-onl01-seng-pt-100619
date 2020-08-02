9ecfb70e43a1cc3ef9b3472b86bdedba973e8a6c
allisoncortez <allizetorc@gmail.com>
2020-08-02 15:18:04 -0500

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+
+  state = {
+    text: ''
+  }
+
+  handleOnChange = (event) => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  handleOnSubmit = (event) => {
+    event.preventDefault()
+    this.props.addRestaurant(this.state.text)
+    // console.log(this.props.addRestaurant(this.state))
+    this.setState({
+      text:''
+    })
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={this.handleOnSubmit}>
+          <input
+            type="text"
+            value={this.state.text}
+            onChange={this.handleOnChange}
+          />
+          <button type="submit">submit</button>
+        </form>
       </div>
     );
   }
--- src/containers/RestaurantsContainer.js
+++ src/containers/RestaurantsContainer.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react'
-import RestaurantInput from '../components/restaurants/RestaurantInput'
-import Restaurants from '../components/restaurants/Restaurants'
+import RestaurantInput from '../components/restaurants/RestaurantInput' //create new restaurants
+import Restaurants from '../components/restaurants/Restaurants' //displays list of restaurants
+import { connect } from 'react-redux'
+//connects to Redux, passes appropriate props down to it's children
 
 class RestaurantsContainer extends Component {
 
   render() {
     return (
       <div>
-        <RestaurantInput />
-        <Restaurants />
+        <RestaurantInput addRestaurant={this.props.addRestaurant}/>
+        <Restaurants
+        restaurants={this.props.restaurants}
+        deleteRestaurant={this.props.deleteRestaurant}/>
       </div>
     )
   }
 }
 
-export default RestaurantsContainer
+const mapStateToProps = state => {
+  return {restaurants: state.restaurants}
+}
+
+const mapDispatchToProps = dispatch => ({
+  addRestaurant: text => dispatch({ type: 'ADD_RESTAURANT', text }),
+  deleteRestaurant: id => dispatch({ type:'DELETE_RESTAURANT', id })
+})
+
+export default connect(mapStateToProps, mapDispatchToProps) (RestaurantsContainer)
